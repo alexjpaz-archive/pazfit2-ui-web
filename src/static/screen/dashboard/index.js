8dfc8548bbab14426a029a900ec1bad9036d213f
Alex Paz <alexjpaz@gmail.com>
2014-11-18 12:17:50 -0500

    dashboard

--- src/static/screen/dashboard/index.js
+++ src/static/screen/dashboard/index.js
@@ -1,10 +1,45 @@
 angular.module('app').config(function(ScreenProvider) {
 	ScreenProvider.register('screen-dashboard-index', {
 		ScreenTitle: 'Dashboard',
-		controller: function($scope, Restangular) {
-			Restangular.all('log').getList().then(function(logs) {
+		controller: function($scope, Restangular, Profile) {
 
-			});
+			function getLatestMax() { 
+				var query = {
+					max_results: 1,
+					page: 1,
+					where: {
+						press: { '$lte' : 90 }
+
+					}
+				};	
+				Restangular.all('max').getList(query).then(function(maxes) {
+					$scope.latestMax = maxes[0];
+				});
+			}
+			
+			function getLatestLogs() {
+				$scope.latestLog = {};
+
+				angular.forEach(Profile.lifts, function(v,lift) {
+					var query = {
+						max_results: 1,
+						page: 1,
+						where: {},
+						sort: '-date'
+					};
+
+					//query.where[lift] = {
+						//date: { '$
+					//};
+
+					Restangular.all('log').getList(query).then(function(log) {
+						$scope.latestMax[lift] = log;
+					});
+				});
+			}
+
+			getLatestLogs();
+			getLatestMax();
 		}
 	});
 });
