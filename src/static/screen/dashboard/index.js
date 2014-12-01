573ccd85a1ba70f1432e1961da43e63dbdd29e88
Alex Paz <alexjpaz@gmail.com>
2014-12-01 09:17:36 -0500

    dashboard

--- src/static/screen/dashboard/index.js
+++ src/static/screen/dashboard/index.js
@@ -2,6 +2,7 @@ angular.module('app').config(function(ScreenProvider) {
 	ScreenProvider.register('screen-dashboard-index', {
 		ScreenTitle: 'Dashboard',
 		controller: function($scope, Restangular, Profile, moment) {
+			$scope.charts = {};
 
 			function getLatestMax() { 
 				var query = {
@@ -23,7 +24,7 @@ angular.module('app').config(function(ScreenProvider) {
 
 				angular.forEach(Profile.lifts, function(v,lift) {
 					var query = {
-						max_results: 1,
+						max_results: 30,
 						page: 1,
 						where: {
 							lift: lift
@@ -33,6 +34,8 @@ angular.module('app').config(function(ScreenProvider) {
 
 					Restangular.all('log').getList(query).then(function(log) {
 						$scope.latestLog[lift] = log[0];
+
+						updateChart(log, lift);
 					});
 
 					query.sort = '-calculated.estimatedMax,-date';
@@ -42,7 +45,7 @@ angular.module('app').config(function(ScreenProvider) {
 				});
 			}
 
-			function getLogs() {
+			function updateChart(logs, lift) {
 				var chart = {
 					data: {
 						labels: [],
@@ -90,31 +93,19 @@ angular.module('app').config(function(ScreenProvider) {
 					}
 				};
 
-				var query = {
-					max_results: 30,
-					page: 1,
-					where: {
-						lift: 'bench'
-					},
-					sort: '-date'
-				};
-
-				Restangular.all('log').getList(query).then(function(logs) {
 					angular.forEach(logs, function(log, i) {
 						var str = moment(log.date).format('YYYY-MM-DD').toString();
 						chart.data.labels.unshift(str);
 						chart.data.datasets[0].data.unshift(log.calculated.estimatedMax);
 						chart.data.datasets[1].data.unshift(log.calculated.effectiveMax);
 						chart.data.datasets[2].data.unshift(log.calculated.targetMax);
-						$scope.chart = chart;
+						$scope.charts[lift] = chart;
 					});
-				});
 
 			}
 
 			getLatestLogs();
 			getLatestMax();
-			getLogs();
 		}
 	});
 });
