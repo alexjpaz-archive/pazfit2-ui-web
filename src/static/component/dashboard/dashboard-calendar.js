e5609b7e21c68973c49db95f2913ea014cb34612
Alex Paz <alexjpaz@gmail.com>
2014-12-04 10:27:07 -0500

    attempting 3 month view

--- src/static/component/dashboard/dashboard-calendar.js
+++ src/static/component/dashboard/dashboard-calendar.js
@@ -1,5 +1,31 @@
 angular.module('app').config(function(ComponentProvider, Chart) {
 	ComponentProvider.register('dashboard-calendar', {
 		componentGroup: 'dashboard',
+		scope: true,
+		controller: function($scope, moment) {
+			var currentMoment = moment();
+			function buildClndr(offset) {
+				var c = {};
+
+				if(angular.isNumber(offset)) {
+					m = currentMoment.clone().add(offset, 'month');
+				} else {
+					m = currentMoment.clone();
+				}
+
+				c.startWithMonth = m;
+
+				return c;
+			}
+			$scope.clndrs = {
+				previous: buildClndr(-1),
+				current: buildClndr(),
+				next: buildClndr(1)
+			};
+			$scope.events = [
+				{ date: "2014-12-01", and: "anything else" }
+			]
+
+		}
 	});
 });
