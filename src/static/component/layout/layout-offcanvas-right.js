0fe5f0d8a0d5ac24012dc825178468f4dddf9b0c
Alex Paz <alexjpaz@gmail.com>
2014-12-06 12:38:48 -0500

    clck for setting canvas

--- src/static/component/layout/layout-offcanvas-right.js
+++ src/static/component/layout/layout-offcanvas-right.js
@@ -1,6 +1,11 @@
 angular.module('app').config(function(ComponentProvider,StringHelper) {
 	ComponentProvider.register('layout-offcanvas-right', {
 		componentGroup: 'layout',
-		replace: true
+		replace: true,
+		link: function(scope, element) {
+			element.find('a').bind('click', function() {
+				element.offcanvas('hide');
+			});
+		}
 	});
 });
